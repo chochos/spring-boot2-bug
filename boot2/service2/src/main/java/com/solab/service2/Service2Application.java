46849350341eb27d4778bf60d71d5ef112ca67fd
Enrique Zamudio <alterbass@gmail.com>
2018-09-25 13:12:57 -0500

    oops

--- boot2/service2/src/main/java/com/solab/service2/Service2Application.java
+++ boot2/service2/src/main/java/com/solab/service2/Service2Application.java
@@ -1,4 +1,4 @@
-package com.solab.service1;
+package com.solab.service2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.core.env.Environment;
 
 import com.solab.lib.SharedClass;
 import com.solab.service1.Other;
+import com.solab.service1.ComponentOne;
 
 @SpringBootApplication
 public class Service2Application {
@@ -15,7 +16,11 @@ public class Service2Application {
     public SharedClass sharedClass(Environment env) {
         return new SharedClass(env.getProperty("shared.value", "default"));
     }
-
+    @Bean
+    public ComponentOne componentOne() {
+        return new ComponentOne();
+    }
+    @Bean
     public Other other() {
         return new Other();
     }
